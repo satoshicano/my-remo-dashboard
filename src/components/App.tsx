dfad1e2847bb3b017d117fedee294545f93278f3
satoshicano <satoshicano@gmail.com>
2020-07-15 00:42:52 +0900

    fix: remove unused imports

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
